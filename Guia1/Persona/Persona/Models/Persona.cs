a9c98879ec746ea33ad0e15d7334b48e8d134ffc
AlexHernandez2698632494 <133897289+AlexHernandez2698632494@users.noreply.github.com>
2024-09-06 19:48:45 -0600

    Guia 01

---
+++ Guia1/Persona/Persona/Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿namespace Persona.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DUI { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string direccion { get; set; }
+        public string correo { get; set; }
+        public string apellido { get; set; }
+
+    }
+}
---
+++ Guia1/Persona/Persona/Models/PersonasDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persona.Models
+{
+    public class PersonasDBContext : DbContext
+        {
+            public PersonasDBContext(DbContextOptions options) : base(options)
+            {
+            }
+            public DbSet<Persona> Peliculas { get; set; }
+        }
+    }
+}
+}

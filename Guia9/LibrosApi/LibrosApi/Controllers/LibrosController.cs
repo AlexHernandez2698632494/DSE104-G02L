6ced090ca0581497bdaa0f5cc1215ebef4852f1d
AlexHernandez2698632494 <133897289+AlexHernandez2698632494@users.noreply.github.com>
2024-09-12 16:10:05 -0600

    update librosApi

---
+++ Guia9/LibrosApi/LibrosApi/Controllers/LibrosController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LibrosApi.Models;
+
+namespace LibrosApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibrosController : ControllerBase
+    {
+        private readonly LibroDbContext _context;
+        public LibrosController(LibroDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Libro>> PostLibro(Libro libro)
+        {
+            if (string.IsNullOrEmpty(libro.Titulo))
+            {
+                return BadRequest("El titulo no puede estar vacio.");
+            }
+            _context.Libros.Add(libro);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetLibro),new {id = libro.Id}, libro);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Libro>> GetLibro(int id)
+        {
+            var libro = await _context.Libros.FindAsync(id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+            return libro;
+        }
+    }
+}
---
+++ Guia9/LibrosApi/LibrosApi/Models/LibroDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibrosApi.Models
+{
+    public class LibroDbContext : DbContext
+    {
+        public LibroDbContext(DbContextOptions<LibroDbContext> options) : base(options) { }
+        public DbSet<Libro> Libros { get; set; }
+    }
+}
